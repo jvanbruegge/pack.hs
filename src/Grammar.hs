2e5034a9f36a8bbeda36cfde984b60a3d73e33be
Jan van Brügge <supermanitu@gmail.com>
2017-05-21 19:52:23 +0200

    Finish Grammar ADT

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -6,46 +6,48 @@ test = "function test4(peter :number, jsd: number, jsdas, sda : string) : boolea
     ++ "}"
 
 newtype Grammar = Grammar [Rule]
-data Rule = Rule String [Production]
+data Rule = Rule String Production
 newtype Production = Production [ProductionPart]
 data ProductionPart = Terminal String
-                    | OptionalTerminal String
+                    | NonTerminal Rule
+                    | Optional Production
+                    | Alternative Production Production
+                    | Group [Production]
+                    | Many Production
+                    | Many1 Production
+                    | Repetition Int Production
                     | Identifier
                     | Literal
-                    | Epsilon
-                    | SubProd Rule
-                    | SubProdList Rule
-                    | OptionalProd Rule
 
 join :: Show a => String -> [a] -> String
 join _ [] = ""
-join char (h:t) =
-    let helper [] = ""
-        helper (x:xs) = char ++ show x ++ helper xs
-    in show h ++ helper t
+join _ (t:[]) = show t
+join char (h:t) = show h ++ char ++ join char t
 
 instance Show Grammar where
     show (Grammar l) = join "\n" l
 
 instance Show Rule where
-    show (Rule name prods) = name ++ " ::= " ++ join " | " prods
+    show (Rule name prod) = name ++ " ::= " ++ show prod
 
 instance Show Production where
     show (Production l) = join " " l
 
 instance Show ProductionPart where
     show (Terminal s) = "'" ++ s ++ "'"
-    show (OptionalTerminal s) = "['" ++ s ++ "']"
+    show (NonTerminal (Rule n _)) = n
+    show (Optional p) = "[" ++ show p ++ "]"
+    show (Alternative a b) = show a ++ " | " ++ show b
+    show (Group l) = "(" ++ join " " l ++ ")"
+    show (Many p) = show p ++ "*"
+    show (Many1 p) = show p ++ "+"
+    show (Repetition n p) = show n ++ "*" ++ show p
     show Identifier = "IDENTIFIER"
     show Literal = "LITERAL"
-    show Epsilon = "EPSILON"
-    show (SubProd (Rule name _)) = name
-    show (SubProdList (Rule name _)) = "{" ++ name ++ "}"
-    show (OptionalProd (Rule name _)) = "[" ++ name ++ "]"
 
 typescript :: Grammar
 typescript = Grammar [
-        Rule "S" [ (Production [SubProd function]) ],
+        Rule "S" $ Production [NonTerminal function],
         function,
         arguments,
         additionalArguments,
@@ -60,56 +62,88 @@ typescript = Grammar [
     ]
 
 
-function = Rule "function" [
-        (Production [Terminal "function", Identifier, Terminal "(", OptionalProd arguments, Terminal ")", OptionalProd tsTypeDeclaration, SubProd block])
+function = Rule "function" $ Production [
+        Terminal "function",
+        Identifier,
+        Terminal "(",
+        Optional $ Production [NonTerminal arguments],
+        Terminal ")",
+        Optional $ Production [NonTerminal tsTypeDeclaration],
+        NonTerminal block
     ]
 
-arguments = Rule "arguments" [
-        (Production [SubProd argument, OptionalProd additionalArguments])
+arguments = Rule "arguments" $ Production [
+        NonTerminal argument,
+        Optional $ Production [NonTerminal additionalArguments]
     ]
 
-additionalArguments = Rule "additionalArguments" [
-        (Production [Terminal ",", SubProd argument, OptionalProd additionalArguments])
+additionalArguments = Rule "additionalArguments" $ Production [
+        Terminal ",",
+        NonTerminal argument,
+        Optional $ Production [NonTerminal additionalArguments]
     ]
 
-argument = Rule "argument" [
-        (Production [Identifier, SubProd tsTypeDeclaration]),
-        (Production [Identifier])
+argument = Rule "argument" $ Production [
+        Identifier,
+        Optional $ Production [NonTerminal tsTypeDeclaration]
     ]
 
-block = Rule "block" [
-        (Production [Terminal "{", SubProdList statement, Terminal "}"])
+block = Rule "block" $ Production [
+        Terminal "{",
+        Many $ Production [NonTerminal statement],
+        Terminal "}"
     ]
 
-tsType = Rule "type" [
-        (Production [Terminal "boolean"]),
-        (Production [Terminal "string"]),
+tsType = Rule "type" $ Production [
+    Alternative
+        (Production [Alternative
+            (Production [Terminal "boolean"])
+            (Production [Terminal "string"])
+        ])
         (Production [Terminal "number"])
     ]
 
-tsTypeDeclaration = Rule "typeDeclaration" [
-        (Production [Terminal ":", SubProd tsType])
+tsTypeDeclaration = Rule "typeDeclaration" $ Production [
+        Terminal ":",
+        NonTerminal tsType
     ]
 
-statement = Rule "statement" [
-        (Production [Terminal "return", SubProd expression, OptionalTerminal ";"])
+statement = Rule "statement" $ Production [
+        Terminal "return",
+        NonTerminal expression,
+        Optional $ Production [Terminal ";"]
     ]
 
-literal = Rule "literal" [
-        (Production [Terminal "\"", Literal, Terminal "\""]),
-        (Production [Literal]),
-        (Production [Terminal "true"]),
-        (Production [Terminal "false"])
+literal = Rule "literal" $ Production [
+    Alternative
+        (Production [Alternative
+            (Production [Terminal "\"", Literal, Terminal "\""])
+            (Production [Literal])
+        ])
+        (Production [Alternative
+            (Production [Terminal "true"])
+            (Production [Terminal "false"])
+        ])
     ]
 
-expression = Rule "expression" [
-        (Production [Identifier]),
-        (Production [SubProd literal]),
-        (Production [SubProd expression, SubProd operator, SubProd expression])
+expression = Rule "expression" $ Production [
+    Alternative
+        (Production [Alternative
+            (Production [Identifier])
+            (Production [NonTerminal literal])
+        ])
+        (Production [
+            NonTerminal expression,
+            NonTerminal operator,
+            NonTerminal expression
+        ])
     ]
 
-operator = Rule "operator" [
-        (Production [Terminal "+"]),
-        (Production [Terminal "-"]),
+operator = Rule "operator" $ Production [
+    Alternative
+        (Production [Alternative
+            (Production [Terminal "+"])
+            (Production [Terminal "-"])
+        ])
         (Production [Terminal "==="])
     ]
